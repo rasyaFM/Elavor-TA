eb6866989ad5b9e93a0ecad367840ce5d0b2c3b9
Muh Fadhli Rahmansyah <78081334+rasyaFM@users.noreply.github.com>
2024-02-21 01:31:32 +0700

    update db

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const user = process.env.MONGO_USER;
 const pass = process.env.MONGO_PASS;
 
-const uri = `mongodb+srv://${user}:${pass}elavorta.8ap31ja.mongodb.net/elavor-ta`;
+const uri = `mongodb+srv://${user}:${pass}elavorta.8ap31ja.mongodb.net/elavorta`;
 
 
 const connectMongoDB = async () => {
